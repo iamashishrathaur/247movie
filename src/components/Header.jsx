5f6cfc2de6c4f54bf52733731e0c8c16e082f977
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-07-06 14:55:58 +0530

    update css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Logo from '../assets/logo.png'
 const Header = () => {
   return (
     <header className='header px-0 lg:px-14' id="header">
-      <div className='header-container'>
+      <div className='header-container mx-0 lg:mx-6'>
         <div className='header-logo p-3'>
            <a className='navbar-brand' href="">
             <img className='logo_img' width={'10em'}  src={Logo} alt="Logo" /></a>
